105a08ea2f758dbf3b663f328134e8c9f1665603
Richard Rodger <richard@ricebridge.com>
2015-05-11 09:38:00 +0000

    first commit

---
+++ system.js
@@ -0,0 +1,19 @@
+// Define the system topology here. The topolgy should reference 
+// containers defined in definitions/services.js
+
+exports.name = 'salestax';
+exports.namespace = 'salestax';
+exports.id = '48cd800f-73a1-4561-9b67-a2a339991911';
+
+exports.topology = {
+  development: {
+  }
+};
+
+// Example
+//
+// exports.topology = {
+//   development: {
+//     root: ['web']
+//   }
+// };
